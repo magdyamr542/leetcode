42dea0f4081347a7b562a6ad1cabb19fcd1815b7
amr.metwally <magdyamr542@gmail.com>
2022-07-22 00:07:52 +0200

    update get num decodings. use dynamic programming

--- decode-ways/main.py
+++ decode-ways/main.py
@@ -5,31 +5,29 @@ def numDecodings(s: str) -> int:
     if len(s) == 1:
         return 0 if s[0] == '0' else 1
 
-    cache = {}
-        
-    def get_num_decoding(curr_word):
-        if cache.get(curr_word , None):
-            return cache.get(curr_word)
+    dp = [0 for i in range(len(s))]
 
-        length = len(curr_word)
-        if length == 0:
-            return 1 # we have built a combination
-        elif len(curr_word) == 1:
-            return 0 if curr_word[0] == '0' else 1
-        
-        using_first_char =  get_num_decoding(curr_word[1:]) if curr_word[0] != '0' else 0
-        using_first_two_chars =   get_num_decoding(curr_word[2:]) if curr_word[0] != '0' and int(curr_word[0:2]) <= 26 else 0
+    for i in range(len(dp) - 1 , -1 , -1):
+        if s[i] == '0':
+            dp[i] = 0
+            continue
+        if i == len(dp) - 1:
+            dp[i] = 1 if s[i] != '0' else 0
+        else:
+            if dp[i+1] != 0:
+                dp[i] += dp[i+1] # taking one char
+            if int(s[i] + s[i+1]) <= 26:
+                if i+2 < len(s):
+                    dp[i] += dp[i+2] # taking two chars
+                else:
+                    dp[i] += 1
 
-        cache[curr_word] =  using_first_char + using_first_two_chars
-        
-        return using_first_char + using_first_two_chars
-        
-    return get_num_decoding(s)
+    return dp[0]
         
     
 
 if __name__ == "__main__":
-    print(numDecodings("122"))
+    print(numDecodings("11106"))
 
 
     
