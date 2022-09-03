5e9edc38a7f70d8f3d8322a7ddfb02eda8446800
amr.metwally <magdyamr542@gmail.com>
2022-09-03 14:27:42 +0200

    updated reverse-bits

--- reverse-bits/main.py
+++ reverse-bits/main.py
@@ -1,9 +1,7 @@
 def reverseBits(n : int):
     print("input=" , "{0:b}".format(n))
     result = 0
-    count = 0
-    while n != 0:
-        count += 1
+    for _ in range(32):
         if n % 2 == 0:
             # most left bit is zero
             result *= 2
@@ -12,11 +10,7 @@ def reverseBits(n : int):
             result = result * 2 + 1
         n = n >> 1 
         
-    while count < 32:
-        count += 1
-        result *= 2
 
-    print("count=" , count)
     print("output=" , "{0:b}".format(result))
     print("output=" , result)
 
@@ -27,7 +21,7 @@ def reverseBits(n : int):
                 
 # https://leetcode.com/problems/reverse-bits/
 if __name__ == "__main__":
-    print(reverseBits(43261596))
+    print(reverseBits(4294967293))
 
 
     
